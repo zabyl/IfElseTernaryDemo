e0b2ab42974b6c371219fe5801c184212be265f9
UgrasK <ugurkaraer1989@gmail.com>
2023-08-21 14:08:01 +0300

    Add project files.

---
+++ IfElseTernaryDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseTernaryDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time > 6 && time < 18)
+            {
+                Console.WriteLine("Day");
+            }
+            else
+            {
+                Console.WriteLine("Night");
+            }
+
+            string result = time > 18 ? "Dark" : "Bright";
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
